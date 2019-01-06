857e0b95bddb59ef09091b1b85d6e7e0d01b2fa4
Esteban Ramos <esteban.ramos.f@gmail.com>
2019-01-06 19:01:25 -0300

    update . se cambío el home y se establece el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
+    path: 'login',
+    loadChildren: './login/login.module#LoginPageModule'
   },
   {
     path: 'mantenedor',
