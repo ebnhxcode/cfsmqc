24ca3a1dd4e89e7334d4989b0c985a85793b3998
Esteban Ramos <esteban.ramos.f@gmail.com>
2019-01-24 22:59:01 -0300

    update - se cambian las url de las apis

--- src/app/servicios/authbasic/auth-basic-config.ts
+++ src/app/servicios/authbasic/auth-basic-config.ts
@@ -1,8 +1,10 @@
 export const authBasicConfig = {
    produccion: false,
    ssl: true,
-   url_base_prod: 'https://cfsmqc.publicidadorigen.cl',
-   url_base_qa: 'https://cfsmqc.publicidadorigen.cl',
+   url_base_prod: 'https://cfsmqcpreprod.publicidadorigen.cl',
+   url_base_qa: 'https://cfsmqcpreprod.publicidadorigen.cl',
+   //url_base_prod: 'https://cfsmqc.publicidadorigen.cl',
+   //url_base_qa: 'https://cfsmqc.publicidadorigen.cl',
    headers: {'Content-Type': 'application/json'},
    //headers: new Headers({'Content-Type': 'application/json'}),
    //options: new RequestOptions({ headers: {'Content-Type': 'application/json'} }),
--- src/app/servicios/authbasic/auth-passport-config.ts
+++ src/app/servicios/authbasic/auth-passport-config.ts
@@ -1,13 +1,25 @@
 export const authPassportConfig = {
    produccion: false,
    ssl: true,
-   url_base_prod: 'https://cfsmqc.publicidadorigen.cl',
-   url_base_qa: 'https://cfsmqc.publicidadorigen.cl',
+   url_base_prod: 'https://cfsmqcpreprod.publicidadorigen.cl',
+   url_base_qa: 'https://cfsmqcpreprod.publicidadorigen.cl',
+   //url_base_prod: 'https://cfsmqc.publicidadorigen.cl',
+   //url_base_qa: 'https://cfsmqc.publicidadorigen.cl',
    headers: {'Content-Type': 'application/json; charset=UTF-8', 'withCredentials': true},
 
+   /* Preprod */
+   apiRoot: 'https://cfsmqcpreprod.publicidadorigen.cl',
+   clientId: 5,
+   clientSecret: 'jy6taFrioQgbfftl5scSl0l2Vo1ZVRSTV6DTiLPu'
+
+   /* Prod */
+   /*
    apiRoot: 'https://cfsmqc.publicidadorigen.cl',
    clientId: 5,
    clientSecret: 'jy6taFrioQgbfftl5scSl0l2Vo1ZVRSTV6DTiLPu'
+   */
+
+
    //headers: new Headers({'Content-Type': 'application/json'}),
    //options: new RequestOptions({ headers: {'Content-Type': 'application/json'} }),
 }
